7f8cc1b033bd63f6ba3d8d391e8f4ee31a50906c
Beth Yue Shi <bethyueshi@Beths-MacBook-Air.local>
2016-02-22 03:13:30 -0800

    add webview of the example map from leaflet

--- app/src/main/java/com/bethyueshi/oasis2/MapActivity.java
+++ app/src/main/java/com/bethyueshi/oasis2/MapActivity.java
@@ -3,19 +3,35 @@ package com.bethyueshi.oasis2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MapActivity extends AppCompatActivity {
 
+    private WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
+
+        mWebView = (WebView)findViewById(R.id.map);
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        //webSettings.setGeolocationEnabled(true);
+
+        mWebView.loadUrl("http://leafletjs.com/examples/mobile-example.html");
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
