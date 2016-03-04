532c9c3066be9baa67055f6e14697e73533683ae
Beth Yue Shi <bethyueshi@Beths-MacBook-Air.local>
2016-03-03 18:26:15 -0800

    on click for video instruction button

--- app/src/main/java/com/bethyueshi/oasis2/VideoFragment.java
+++ app/src/main/java/com/bethyueshi/oasis2/VideoFragment.java
@@ -1,13 +1,16 @@
 package com.bethyueshi.oasis2;
 
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class VideoFragment extends Fragment {
+public class VideoFragment extends Fragment implements View.OnClickListener {
     private GIFView instr;
 
     @Override
@@ -16,6 +19,7 @@ public class VideoFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.video_fragment, container, false);
 
         instr = (GIFView)rootView.findViewById(R.id.test_pic_large);
+        instr.setOnClickListener(this);
 
         //Bundle args = getArguments();
         //Log.d("haha", " " + args.getInt("resId"));
@@ -27,5 +31,20 @@ public class VideoFragment extends Fragment {
     public void setGIF(int resId, int start){
         instr.setSrc(resId);
         instr.setStart(start);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("instr", "there");
+        switch (v.getId()) {
+            case R.id.test_pic_large:
+                Intent intent = new Intent(getActivity(), CameraActivity.class);
+                Log.d("instr", "here");
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
     }
 }
