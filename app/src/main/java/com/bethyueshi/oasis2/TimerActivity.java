3ec06de653158a0203b3bd6940fac0b3931fa218
Beth Yue Shi <bethyueshi@Beths-MacBook-Air.local>
2016-03-15 04:00:19 -0700

    test timer based on test number

--- app/src/main/java/com/bethyueshi/oasis2/TimerActivity.java
+++ app/src/main/java/com/bethyueshi/oasis2/TimerActivity.java
@@ -21,8 +21,10 @@ public class TimerActivity extends AppCompatActivity {
     private Button btnTimer;
     private TextView textTimer;
     private CountDownTimer countDownTimer;
-    private int wait = 1; // minutes to wait
-    public static final int SIXTY = 10; //TODO change it back to 60
+    private double wait = 0.1; // minutes to wait
+    public static final int SIXTY = 60; //TODO change it back to 60
+    private int seconds = (int)(SIXTY * wait);
+    private int testNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +35,14 @@ public class TimerActivity extends AppCompatActivity {
         textTimer = (TextView)findViewById(R.id.textTimer);
         btnTimer = (Button)findViewById(R.id.button_start);
 
-        barTimer.setMax(SIXTY * wait);
-        barTimer.setProgress(SIXTY * wait);
-        barTimer.setSecondaryProgress(SIXTY * wait);
-        textTimer.setText(wait + ":00");
+        testNum = getIntent().getIntExtra("test_num", 0);
+        setTimer(testNum);
+
+        barTimer.setMax(seconds);
+        barTimer.setProgress(seconds);
+        barTimer.setSecondaryProgress(seconds);
+        textTimer.setText(String.format("%02d", (int)(Math.floor(wait))) + ":" +
+                String.format("%02d", seconds % SIXTY));
 
         btnTimer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,20 +54,41 @@ public class TimerActivity extends AppCompatActivity {
         });
     }
 
-    public void setWaitTime(int m){
+    public void setWaitTime(double m){
         this.wait = m;
     }
 
-    private void startTimer(final int m) {
-        countDownTimer = new CountDownTimer(SIXTY * m * 1000, 100) {
+    public void setTimer(int testNum){
+        switch(testNum){
+            case 0:
+                wait = 0.5;
+                break;
+            case 1:
+                wait = 0.1;
+                break;
+            case 2:
+                wait = 0.3;
+                break;
+            case 3:
+                wait = 0.2;
+                break;
+            default:
+                wait = 1;
+        }
+
+        seconds = (int)(SIXTY * wait);
+    }
+
+    private void startTimer(final double m) {
+        countDownTimer = new CountDownTimer((int)(SIXTY * m * 1000), 100) {
             // 500 means, onTick function will be called at every 500 milliseconds
 
             @Override
             public void onTick(long leftTimeInMilliseconds) {
-                long seconds = leftTimeInMilliseconds / 1000;
-                barTimer.setSecondaryProgress((int)seconds);
-                textTimer.setText(String.format("%02d", seconds / SIXTY) + ":" +
-                                  String.format("%02d", seconds % SIXTY));
+                long secs = leftTimeInMilliseconds / 1000;
+                barTimer.setSecondaryProgress((int)secs);
+                textTimer.setText(String.format("%02d", secs / SIXTY) + ":" +
+                                  String.format("%02d", secs % SIXTY));
                 // format the textview to show the easily readable format
             }
 
@@ -69,12 +96,13 @@ public class TimerActivity extends AppCompatActivity {
             public void onFinish() {
                 if(textTimer.getText().equals("00:00")){
                     Intent intent = new Intent(TimerActivity.this, CameraActivity.class);
-                    intent.putExtra("test_num", getIntent().getIntExtra("test_num", 0));
+                    intent.putExtra("test_num", testNum);
                     startActivity(intent);
                 }
                 else{
-                    textTimer.setText(wait + ":00");
-                    barTimer.setSecondaryProgress(SIXTY * m);
+                    textTimer.setText(String.format("%02d", (int)(Math.floor(m))) + ":" +
+                                      String.format("%02d", seconds % SIXTY));
+                    barTimer.setSecondaryProgress((int)(SIXTY * m));
                 }
             }
         }.start();
